1cb13f911c66e7fb73e992fd88bf84a0d93a0db4
OatsProduction <oatsprod@gmail.com>
2022-05-06 13:40:44 +0200

    working clustering of data WIP of training

--- training/ClustBert.py
+++ training/ClustBert.py
@@ -1,7 +1,9 @@
 import string
 
 import torch
-from transformers import BertTokenizer, BertModel
+from datasets import Dataset
+from sklearn.cluster import KMeans
+from transformers import BertTokenizer, BertModel, TrainingArguments, Trainer
 import torch.nn as nn
 
 
@@ -9,13 +11,23 @@ class ClustBERT(nn.Module):
 
     def __init__(self):
         super(ClustBERT, self).__init__()
-
-        self.encoder = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+        self.model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    def get_sentence_vector_from_cls(self, text):
-        return torch.LongTensor(self.tokenizer.encode(text))
-        # return self.tokenizer.convert_ids_to_tokens(ids)
+    def get_sentence_vectors_with_token_average(self, texts: list):
+        return [self.get_sentence_vector_with_token_average(text) for text in texts]
+
+    def get_sentence_vector_with_token_average(self, text):
+        tokens = torch.LongTensor(self.tokenizer.encode(text))
+        tokens = tokens.to(device)
+        tokens = tokens.unsqueeze(0)
+
+        with torch.no_grad():
+            out = clust_bert.model(input_ids=tokens)
+
+        # we only want the hidden_states
+        hidden_states = out[2]
+        return torch.mean(hidden_states[-1], dim=1).squeeze()
 
 
 def preload(file: string) -> []:
@@ -28,21 +40,24 @@ def preload(file: string) -> []:
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+clust_bert = ClustBERT().to(device)
+clust_bert.model.eval()
+
 result = preload('../dataset/News/train.csv')
-model = ClustBERT().to(device)
-model.encoder.eval()
+test_data = ["This is cool", "believe me.", "Let's try Bert."]
+sentence_embedding = clust_bert.get_sentence_vectors_with_token_average(test_data)
+X = [sentence.cpu().detach().numpy() for sentence in sentence_embedding]
 
-tokens = model.get_sentence_vector_from_cls("This is cool")
-tokens = tokens.to(device)
-tokens = tokens.unsqueeze(0)
+k = 2
+k_means = KMeans(k)
+pseudo_labels = k_means.fit_predict(X)
 
-with torch.no_grad():
-    out = model.encoder(input_ids=tokens)
+train_encodings = clust_bert.tokenizer(test_data, truncation=True, padding=True)
+my_dict = {"0": test_data, "1": ["Think about this."]}
+dataset = Dataset.from_dict(my_dict)
+print(dataset)
 
-# the output is a tuple
-print(type(out))
-# the tuple contains three elements as explained above)
-print(out)
-# we only want the hidden_states
-hidden_states = out[2]
-print(len(hidden_states))
+training_args = TrainingArguments(output_dir="test_trainer")
+trainer = Trainer(model=clust_bert, args=training_args, train_dataset=dataset)
+
+trainer.train()
