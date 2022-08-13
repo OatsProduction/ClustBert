22b603cfb0ef17d2bd762c7f59493aad5af9c2b8
OatsProduction <oatsprod@gmail.com>
2022-08-13 03:31:19 +0200

    added an all option to the evaluation

--- evaluation/evaluate_model.py
+++ evaluation/evaluate_model.py
@@ -25,12 +25,15 @@ def batcher(params, batch):
 
 # Set up logger
 logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+sts = "STS12,STS13,STS14,STS15,STS16"
+seneval_tasks = "MR,CR,SUBJ,MPQA,SST2,SST5,TREC,MRPC"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, help="define the path to model to load")
     parser.add_argument("--sts", help="perform all STS", action="store_true")
     parser.add_argument("--senteval", help="perform all SentEval Tests", action="store_true")
+    parser.add_argument("--all", help="perform all evaluation Tests", action="store_true")
     parser.add_argument("--senteval_path", type=str, help="define the path to model to load")
     parser.add_argument('--device', type=str, help='the device used by the program. Default is cuda:0')
     args = parser.parse_args()
@@ -39,9 +42,11 @@ if __name__ == '__main__':
     print("Started the evaluation script with the device: " + str(device))
 
     if args.model is not None:
-        transformer = pickle.load(open(args.model, 'rb'))
+        model_name = args.model
+        transformer = pickle.load(open("../output/" + args.model, 'rb'))
         transformer.to(device=device)
     else:
+        model_name = "plain_bert"
         transformer = BertModel.from_pretrained("bert-base-cased", output_hidden_states=True)
         transformer.eval()
         transformer.to(device=device)
@@ -51,23 +56,28 @@ if __name__ == '__main__':
     params = {
         'task_path': senteval_path,
         'usepytorch': True,
-        'kfold': 5
+        'kfold': 5,
+        'classifier': {
+            'nhid': 0,
+            'optim': 'rmsprop',
+            'batch_size': 128,
+            'tenacity': 3,
+            'epoch_size': 2
+        }
     }
-    params['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
-                            'tenacity': 3, 'epoch_size': 2}
 
     se = senteval.engine.SE(params, batcher, prepare)
 
     if args.sts:
-        sts = "STS12,STS13,STS14,STS15,STS16"
         transfer_tasks = sts.split(",")
     if args.senteval:
-        seneval_tasks = "MR,CR,SUBJ,MPQA,SST2,SST5,TREC,MRPC"
         transfer_tasks = seneval_tasks.split(",")
+    if args.all:
+        transfer_tasks = (sts + "," + seneval_tasks).split(",")
     else:
         transfer_tasks = ["SNLI"]
 
-    print("Started the tasks")
-
     results = se.eval(transfer_tasks)
-    print(results)
+
+    with open(model_name + '_evaluation_results.json', 'w') as outfile:
+        outfile.write(results)
