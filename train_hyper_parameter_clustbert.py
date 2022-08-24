f0fd80dfd483f29bad4b74ce016630233dad7ed3
OatsProduction <oatsprod@gmail.com>
2022-08-24 23:26:54 +0200

    working on a sweep setup

--- train_hyper_parameter_clustbert.py
+++ train_hyper_parameter_clustbert.py
@@ -1,25 +1,55 @@
-import torch
 import wandb
-from pytorch_lightning.loggers import WandbLogger
-from transformers import Trainer
+from torch.utils.data import DataLoader
+from transformers import DataCollatorWithPadding
 
 from models.pytorch.ClustBERT import ClustBERT
 from training import DataSetUtils
+from training.PlainPytorchTraining import train_loop
+
+
+def start_train(config=None):
+    # Initialize a new wandb run
+    with wandb.init(config=config):
+        # If called by wandb.agent, as below,
+        # this config will be set by Sweep Controller
+        config = wandb.config
+
+        train = DataSetUtils.get_million_headlines()
+        train = train.select(range(1, 150))
+
+        clust_bert = ClustBERT(config)
+        train = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, train)
+
+        wandb.init(project="test-project", entity="clustbert")
+        wandb.watch(clust_bert)
+
+        data_collator = DataCollatorWithPadding(tokenizer=clust_bert.tokenizer)
+
+        for epoch in range(0, config.epochs):
+            print("Loop in Epoch: " + str(epoch))
+            pseudo_label_data, silhouette = clust_bert.cluster_and_generate(train)
+            # nmi = normalized_mutual_info_score(train["labels"], pseudo_label_data["labels"])
+
+            train_dataloader = DataLoader(
+                pseudo_label_data, shuffle=True, batch_size=8, collate_fn=data_collator
+            )
+            loss = train_loop(clust_bert, train_dataloader)
+
+            wandb.log({
+                "loss": loss,
+                "silhouette": silhouette
+            })
+
+        clust_bert.save()
+
 
 if __name__ == '__main__':
-    wandb.init()
-    wandb_logger = WandbLogger()
-
-    train = DataSetUtils.get_million_headlines()
-    device = torch.device("cuda:0")
-    model = ClustBERT(100, device)
-
-    # setup Trainer
-    trainer = Trainer(
-        logger=wandb_logger,  # W&B integration
-        gpus=-1,  # use all GPU's
-        max_epochs=3  # number of epochs
+    hyperparameter_defaults = dict(
+        learning_rate=0.001,
+        k=100,
+        optimizer="adam",
+        epochs=12,
     )
 
-    # train
-    trainer.fit(model, train)
+    sweep_id = wandb.sweep(config=hyperparameter_defaults, project="ClustBert")
+    wandb.agent(sweep_id, start_train, count=5)
