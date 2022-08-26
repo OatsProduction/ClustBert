f5a5ed26a603b2951ef8e3a63b54cbe6aa99e27c
OatsProduction <oatsprod@gmail.com>
2022-08-27 00:35:45 +0200

    test hyper parameters

--- train_hyper_parameter_clustbert.py
+++ train_hyper_parameter_clustbert.py
@@ -1,53 +1,81 @@
+import senteval
 import torch
-import wandb
 from torch.utils.data import DataLoader
-from transformers import DataCollatorWithPadding
+from transformers import DataCollatorWithPadding, BertTokenizer
 
-from evaluation.evaluate_model import evaluate_model
 from models.pytorch.ClustBERT import ClustBERT
 from training import DataSetUtils
 from training.PlainPytorchTraining import train_loop
 
 
+def prepare(params, samples):
+    return
+
+
+def batcher(params, batch):
+    sentences = [" ".join(s).lower() for s in batch]
+
+    with torch.no_grad():
+        y = params['tokenizer'](sentences, padding=True, truncation=True, return_tensors="pt")["input_ids"]
+        y = params['model'](y)[0]
+        y = y[:, 0, :].view(-1, 768)
+
+    return y
+
+
 def start_train(config=None):
     # Initialize a new wandb run
-    with wandb.init(config=config):
-        # If called by wandb.agent, as below,
-        # this config will be set by Sweep Controller
-        config = wandb.config
-        device = torch.device("cuda:0")
+    # with wandb.init(config=config):
+    # If called by wandb.agent, as below,
+    # this config will be set by Sweep Controller
+    # config = wandb.config
+    device = torch.device("cuda:0")
 
-        train = DataSetUtils.get_million_headlines()
-        train = train.select(range(1, 10))
+    train = DataSetUtils.get_million_headlines()
+    train = train.select(range(1, 10))
 
-        clust_bert = ClustBERT(config.k)
-        clust_bert.to(device)
-        wandb.watch(clust_bert)
+    clust_bert = ClustBERT(5)
+    clust_bert.to(device)
+    # wandb.watch(clust_bert)
 
-        train = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, train)
-        data_collator = DataCollatorWithPadding(tokenizer=clust_bert.tokenizer)
+    train = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, train)
+    data_collator = DataCollatorWithPadding(tokenizer=clust_bert.tokenizer)
 
-        for epoch in range(0, 1):
-            print("Loop in Epoch: " + str(epoch))
-            pseudo_label_data, silhouette = clust_bert.cluster_and_generate(train, device)
-            # nmi = normalized_mutual_info_score(train["labels"], pseudo_label_data["labels"])
+    for epoch in range(0, 1):
+        print("Loop in Epoch: " + str(epoch))
+        pseudo_label_data, silhouette = clust_bert.cluster_and_generate(train, device)
+        # nmi = normalized_mutual_info_score(train["labels"], pseudo_label_data["labels"])
 
-            train_dataloader = DataLoader(
-                pseudo_label_data, shuffle=True, batch_size=8, collate_fn=data_collator
-            )
-            loss = train_loop(clust_bert, train_dataloader, device, config)
+        train_dataloader = DataLoader(
+            pseudo_label_data, shuffle=True, batch_size=8, collate_fn=data_collator
+        )
+        loss = train_loop(clust_bert, train_dataloader, device)
 
-            wandb.log({
-                "loss": loss,
-                "silhouette": silhouette
-            })
+        # wandb.log({
+        #     "loss": loss,
+        #     "silhouette": silhouette
+        # })
 
-        clust_bert.to(torch.device("cpu"))
-        result = evaluate_model(clust_bert.model, ["MR", "CR"], config)
-        wandb.log("MR", result["MR"]["acc"])
-        wandb.log("CR", result["CR"]["acc"])
+    print("Start with SentEval")
 
-        # clust_bert.save()
+    clust_bert.to(torch.device("cpu"))
+    clust_bert.eval()
+
+    params = {
+        'model': clust_bert.model,
+        'tokenizer': BertTokenizer.from_pretrained("bert-base-cased"),
+        'task_path': "../SentEval/data",
+        'usepytorch': True,
+        'kfold': 5,
+        'classifier': {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
+                       'tenacity': 3, 'epoch_size': 2}
+    }
+    se = senteval.engine.SE(params, batcher, prepare)
+    result = se.eval(["CR"])
+    # wandb.log("MR", result["MR"]["acc"])
+    # wandb.log("CR", result["CR"]["acc"])
+    print(result)
+    # clust_bert.save()
 
 
 if __name__ == '__main__':
@@ -66,5 +94,6 @@ if __name__ == '__main__':
             }
         }
     }
-    sweep_id = wandb.sweep(sweep_config, project="test-project")
-    wandb.agent(sweep_id, start_train, count=1)
+    start_train()
+    # sweep_id = wandb.sweep(sweep_config, project="test-project")
+    # wandb.agent(sweep_id, start_train, count=1)
