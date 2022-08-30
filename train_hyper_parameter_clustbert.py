a9b5c519ecc424e9eb0d9709dcec52cd439dc24c
OatsProduction <oatsprod@gmail.com>
2022-08-30 02:30:11 +0200

    next test with sweep.yaml

--- train_hyper_parameter_clustbert.py
+++ train_hyper_parameter_clustbert.py
@@ -1,5 +1,3 @@
-import argparse
-
 import senteval
 import torch
 import wandb
@@ -28,61 +26,65 @@ def batcher(params, batch):
 
 def start_train(config=None):
     # Initialize a new wandb run
-    wandb.run.name = "data_" + str(config.data) + "_lr" + str(config.learning_rate) + "_k" + str(
-        config.k) + "_" + wandb.run.id
+    with wandb.init(config=config):
+        # If called by wandb.agent, as below,
+        # this config will be set by Sweep Controller
+        config = wandb.config
+        wandb.run.name = "data_" + str(config.data) + "_lr" + str(config.learning_rate) + "_k" + str(
+            config.k) + "_" + wandb.run.id
 
-    device = torch.device("cuda:1")
+        device = torch.device("cuda:1")
 
-    big_train_dataset = DataSetUtils.get_million_headlines()
-    big_train_dataset = big_train_dataset.select(range(1, 100000))
+        big_train_dataset = DataSetUtils.get_million_headlines()
+        big_train_dataset = big_train_dataset.select(range(1, 100000))
 
-    clust_bert = ClustBERT(config.k)
-    clust_bert.to(device)
-    wandb.watch(clust_bert)
+        clust_bert = ClustBERT(config.k)
+        clust_bert.to(device)
+        wandb.watch(clust_bert)
 
-    big_train_dataset = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, big_train_dataset)
-    data_collator = DataCollatorWithPadding(tokenizer=clust_bert.tokenizer)
+        big_train_dataset = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, big_train_dataset)
+        data_collator = DataCollatorWithPadding(tokenizer=clust_bert.tokenizer)
 
-    for epoch in range(0, config.epochs):
-        print("Loop in Epoch: " + str(epoch))
-        big_train_dataset.shuffle(seed=epoch)
-        train = big_train_dataset.select(range(1, config.random_crop_size))
+        for epoch in range(0, config.epochs):
+            print("Loop in Epoch: " + str(epoch))
+            big_train_dataset.shuffle(seed=epoch)
+            train = big_train_dataset.select(range(1, config.random_crop_size))
 
-        pseudo_label_data, silhouette = clust_bert.cluster_and_generate(train, device)
-        # nmi = normalized_mutual_info_score(train["labels"], pseudo_label_data["labels"])
+            pseudo_label_data, silhouette = clust_bert.cluster_and_generate(train, device)
+            # nmi = normalized_mutual_info_score(train["labels"], pseudo_label_data["labels"])
 
-        train_dataloader = DataLoader(
-            pseudo_label_data, shuffle=True, batch_size=8, collate_fn=data_collator
-        )
-        loss = train_loop(clust_bert, train_dataloader, device, config)
+            train_dataloader = DataLoader(
+                pseudo_label_data, shuffle=True, batch_size=8, collate_fn=data_collator
+            )
+            loss = train_loop(clust_bert, train_dataloader, device, config)
 
-        wandb.log({
-            "loss": loss,
-            "silhouette": silhouette
-        })
+            wandb.log({
+                "loss": loss,
+                "silhouette": silhouette
+            })
 
-    print("Start with SentEval")
+        print("Start with SentEval")
 
-    clust_bert.to(torch.device("cpu"))
-    clust_bert.eval()
+        clust_bert.to(torch.device("cpu"))
+        clust_bert.eval()
 
-    params = {
-        'model': clust_bert.model,
-        'tokenizer': BertTokenizer.from_pretrained("bert-base-cased"),
-        'task_path': "../SentEval/data",
-        'usepytorch': True,
-        'classifier': {
-            'nhid': 0,
-            'optim': 'adam',
-            'batch_size': 64,
-            'tenacity': 5,
-            'epoch_size': 4
+        params = {
+            'model': clust_bert.model,
+            'tokenizer': BertTokenizer.from_pretrained("bert-base-cased"),
+            'task_path': "../SentEval/data",
+            'usepytorch': True,
+            'classifier': {
+                'nhid': 0,
+                'optim': 'adam',
+                'batch_size': 64,
+                'tenacity': 5,
+                'epoch_size': 4
+            }
         }
-    }
-    se = senteval.engine.SE(params, batcher, prepare)
-    result = se.eval(["MR", "CR"])
-    mr_cr = (float(result["MR"]["acc"]) + float(result["CR"]["acc"])) / 2
-    wandb.log({"MR_CR_score": mr_cr})
+        se = senteval.engine.SE(params, batcher, prepare)
+        result = se.eval(["MR", "CR"])
+        mr_cr = (float(result["MR"]["acc"]) + float(result["CR"]["acc"])) / 2
+        wandb.log({"MR_CR_score": mr_cr})
 
 
 if __name__ == '__main__':
@@ -107,7 +109,6 @@ if __name__ == '__main__':
             }
         }
     }
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    wandb.init(project="clustbert", config=args)
-    start_train(wandb.config)
+
+    sweep_id = wandb.sweep(sweep_config, project="clustbert")
+    wandb.agent(sweep_id, function=start_train, count=5)
