e29b5e102cccb3f81273f2740af857c7e78e59c1
OatsProduction <oatsprod@gmail.com>
2022-09-10 15:17:33 +0200

    next try - with elbow criteria

--- calculate_elbow_criterion.py
+++ calculate_elbow_criterion.py
@@ -1,8 +1,8 @@
 import argparse
 
+import torch
 import wandb
 from sklearn.cluster import MiniBatchKMeans
-from wandb.wandb_torch import torch
 
 from models.pytorch.ClustBERT import ClustBERT
 from training import DataSetUtils
@@ -10,16 +10,13 @@ from training.DataSetUtils import get_million_headlines
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cuda', type=int, help='the index of the cuda GPU. Default is 0')
+    parser.add_argument('--device', type=str, help='the index of the cuda GPU. Default is 0')
     args = parser.parse_args()
 
     wandb.init(project="test-project", entity="clustbert")
-
-    cuda_index = "0" if args.cuda is None else str(args.cuda)
-    device = torch.device('cuda:' + cuda_index if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cpu")
     print("Using device: " + str(device))
     clust_bert = ClustBERT(100)
-    clust_bert.to(device)
 
     dataset = get_million_headlines()
     dataset = DataSetUtils.preprocess_datasets(clust_bert.tokenizer, dataset)
