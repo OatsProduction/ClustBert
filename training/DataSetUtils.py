7a969c30af768bec53912d65a86c00c184018971
OatsProduction <oatsprod@gmail.com>
2022-09-13 21:20:07 +0200

    new test with - added data augmentation

--- training/DataSetUtils.py
+++ training/DataSetUtils.py
@@ -7,18 +7,7 @@ from datasets import load_dataset, Dataset
 from transformers import BertTokenizer
 
 logging.disable(logging.INFO)  # disable INFO and DEBUG logger everywhere
-tuples = [
-    None,
-    naw.SynonymAug(aug_src='wordnet'),
-    naw.ContextualWordEmbsAug(
-        model_path='distilbert-base-uncased', action="substitute", device="cuda"),
-    # naw.RandomWordAug(action='crop'),
-    # naw.RandomWordAug(),
-    naw.ContextualWordEmbsAug(
-        model_path='roberta-base', action="substitute", device="cuda"),
-    naw.ContextualWordEmbsAug(
-        model_path='bert-base-uncased', action="insert", device="cuda"),
-]
+tuples = [None]
 
 
 def get_snli_dataset() -> Union:
@@ -94,8 +83,22 @@ def get_pedia_classes() -> Dataset:
     return dataset
 
 
-def preprocess_datasets(tokenizer: BertTokenizer, data_set: Dataset) -> Dataset:
+def preprocess_datasets(tokenizer: BertTokenizer, data_set: Dataset, device) -> Dataset:
     print("Preprocess the data")
+    global tuples
+    tuples = [
+        None,
+        naw.SynonymAug(aug_src='wordnet'),
+        naw.ContextualWordEmbsAug(
+            model_path='distilbert-base-uncased', action="substitute", device=device),
+        # naw.RandomWordAug(action='crop'),
+        # naw.RandomWordAug(),
+        naw.ContextualWordEmbsAug(
+            model_path='roberta-base', action="substitute", device=device),
+        naw.ContextualWordEmbsAug(
+            model_path='bert-base-uncased', action="insert", device=device),
+    ]
+
     data_set = data_set.map(augment_dataset, batched=True)
 
     data_set = data_set.map(
