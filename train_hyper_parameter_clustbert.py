8dc202d911a908027fc7c3cf79db0d3862f78d96
OatsProduction <oatsprod@gmail.com>
2022-10-07 20:00:57 +0200

    next ClustBERT with Trec data set

--- train_hyper_parameter_clustbert.py
+++ train_hyper_parameter_clustbert.py
@@ -15,7 +15,7 @@ from training.PlainPytorchTraining import train_loop, eval_loop, UnifLabelSample
 
 
 def start_train(config=None):
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:1")
     torch.cuda.empty_cache()
 
     bert_model = "base" if config is None or config.model is None else config.model
